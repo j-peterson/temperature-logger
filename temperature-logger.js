a6213906dcd5849357e1a6a30238a7f76a3e62d0
j-peterson <jtpeterson@mdpnp.org>
2014-12-08 17:56:36 -0500

    It actually works now... fixed errors with negative temperatures. changed regexs for parsing time from serial data. nested sp.open inside fs.open.

--- temperature-logger.js
+++ temperature-logger.js
@@ -6,6 +6,7 @@
     This will collect data from the "main screen" temperature reading in whatever mode and units are active.
     It's recommended that the thermometer be used only be in mode "T1" or "T2" and should not be in modes: "REL", "AVG", "MAX", "MIN", or "T1-T2".
     You must specify the absolute path to the serial port. For example: /dev/ttyO0
+    Turn the thermometer on before starting the script.
 */
 
 var fs = require('fs');
@@ -15,15 +16,35 @@ var moment = require('moment');
 var SerialPort = serialport.SerialPort;
 var device = process.argv[2];
 // var device = "/dev/tty.USA19H141P1.1";
-var serialOpen = false;
+
 function time() {
-  return moment().format('YYYY-MM-DDTHH:mm:ss');
-}
+  return moment().format('YYYY-MM-DD HH:mm:ss');
+};
+function getData() {
+  console.log('Starting to send data to serial port');
+  var timer = setInterval(function () {
+    sp.write('D', function (err, results) {   // send 'D' for thermometer primary window display and 'B' for secondary
+      if(err) { console.log('Error sending command "D" to serial port - ' + err) };
+    });
+  }, 1000);
+};
+function writeToFile(fd, buffer) {
+  buffer = new Buffer(buffer + '\r\n');
+  var offset = 0;
+  var length = buffer.length;
+  var position = null;
+  fs.write(fd, buffer, offset, length, position, function (err, written, buffer) {
+    // console.log(err, written, buffer);
+  });
+};
 
 if (!device) {
   console.log('You must specify the absolute path to a serial port. For example: /dev/ttyO0');
   process.exit();
 } else {
+  var file = device.split('/');
+  file = './' + file[file.length - 1] + '_' + moment().format('YY-MM-DDTHHmm') + '.csv';
+
   var sp = new SerialPort(device, {
     baudrate: 9600,
     databits: 8,
@@ -32,43 +53,35 @@ if (!device) {
     // parser: serialport.parsers.raw
   });
 
-  sp.open(function (error) {
-    if(!error) {
-      serialOpen = true
-      console.log('Opened serial connection at ' + time);
-      
-      sp.on('data', function (data) {
-        console.log(data, time);
-        fs.write(fd, data.match(/\b\d+\.*\d*/)[0] + ',' + data.match(/T|F/)[0] + ',' + time + '\r');
-      });
-      
-      sp.on('close', function (error) {
-        clearInterval(timer);
-        console.log('Closed serial connection at ' + time);
-      });
-    } else {
-      console.log("Error opening serial port - " + error);
-      process.exit();
-    }
-  });
-};
-
-if(serialOpen) {
-  var file = device.split('/');
-  file = file[file.length - 1] + '_' + moment().format('YY-MM-DDTHHmm') + '.csv';
-  fs.open('./' + file, 'w', function (err, fd) {
+  fs.open(file, 'w', function (err, fd) {
     if(!err) {
-      // fs.open callback
-      var timer = setInterval(function () {
-        sp.write('D', function (err, results) {
-          if(err) { console.log('Error sending command "D" to serial port - ' + err) };
-        });
-      }, 1000);
+      sp.open(function (error) {
+        if(!error) {
+          console.log('Opened serial connection with ' + device + ' at ' + time());
+          writeToFile(fd, 'Opened serial connection with ' + device + ' at ' + time());
+          writeToFile(fd, 'Probe Label,Temperature,Units,Time/Date Stamp');
+          
+          sp.on('data', function (data) {
+            console.log(data, time());
+            var d = data.match(/(T[1|2])\s+(-?)\s*(\d+\.?\d*)\s+([C|F])/).splice(1,4);
+            if (d) {
+              writeToFile(fd, d[0] + ',' + d[1] + d[2] + ',' + d[3] + ',' + time());
+            };
+          });
+          
+          sp.on('close', function (error) {
+            clearInterval(timer);
+            console.log('Closed serial connection at ' + time());
+          });
+          // fs.open callback
+          getData();
+        } else {
+          console.log("Error opening serial port - " + error);
+          process.exit();
+        }
+      });
     } else {
-      console.log('Error creating file - ' + err);
+      console.log('Error creating file - ' + fd + ' Error - ' + err);
     };
   });
 };
-
-
-
