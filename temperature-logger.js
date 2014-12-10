db1209e21c2cb59c4872ba54c297abaf3688524c
j-peterson <jtpeterson@mdpnp.org>
2014-12-10 17:54:31 -0500

    added support for custom sampling period from the command line. made the detecting presence of serial device much more robust with fs.existsSync(device).

--- temperature-logger.js
+++ temperature-logger.js
@@ -1,22 +1,34 @@
 /*
   Collect data from the BK Precision 710 RS232 Thermometer to a csv file.
 
-  Usage: node temperature-logger.js /dev/tty.your-serial-device
+  Usage: node temperature-logger.js /dev/tty.your-serial-device [samplePeriod]
+
+    /dev/tty.your-serial-device - You must specify the absolute path to the serial port. For example: /dev/ttyO0
+    [samplePeriod] - You may optionally specify a sampling period, which is the time interval between samples. If null, default is 1 second.
 
     This will collect data from the "main screen" temperature reading in whatever mode and units are active.
     It's recommended that the thermometer be used only be in mode "T1" or "T2" and should not be in modes: "REL", "AVG", "MAX", "MIN", or "T1-T2".
-    You must specify the absolute path to the serial port. For example: /dev/ttyO0
-    Turn the thermometer on before starting the script.
 */
-
 var fs = require('fs');
 var serialport = require('serialport');
 var moment = require('moment');
 
 var SerialPort = serialport.SerialPort;
 var device = process.argv[2];
+var samplePeriod = process.argv[3];
 // var device = "/dev/tty.USA19H141P1.1";
 
+function getSamplePeriod() {
+  if (samplePeriod >= 1 && samplePeriod <= 3600) {
+    console.log('Sending requests with a sampling period of ' + samplePeriod + ' seconds')
+    return samplePeriod * 1000;
+  } else if (samplePeriod === undefined) {
+    return 1000;
+  } else {
+    console.log('Sample period must be a number of seconds between 1 and 3600');
+    process.exit();
+  }
+};
 function time() {
   return moment().format('YYYY-MM-DD HH:mm:ss');
 };
@@ -26,7 +38,7 @@ function getData() {
     sp.write('D', function (err, results) {   // send 'D' for thermometer primary window display and 'B' for secondary
       if(err) { console.log('Error sending command "D" to serial port - ' + err) };
     });
-  }, 1000);
+  }, getSamplePeriod());
 };
 function writeToFile(fd, buffer) {
   buffer = new Buffer(buffer + '\r\n');
@@ -38,10 +50,7 @@ function writeToFile(fd, buffer) {
   });
 };
 
-if (!device) {
-  console.log('You must specify the absolute path to a serial port. For example: /dev/ttyO0');
-  process.exit();
-} else {
+if(device!==null && fs.existsSync(device)) {
   var file = device.split('/');
   file = './' + file[file.length - 1] + '_' + moment().format('YY-MM-DDTHHmm') + '.csv';
 
@@ -66,14 +75,13 @@ if (!device) {
             var d = data.match(/(T[1|2])\s+(-?)\s*(\d+\.?\d*)\s+([C|F])/).splice(1,4);
             if (d) {
               writeToFile(fd, d[0] + ',' + d[1] + d[2] + ',' + d[3] + ',' + time());
-            };
+            }
           });
           
           sp.on('close', function (error) {
             clearInterval(timer);
             console.log('Closed serial connection at ' + time());
           });
-          // fs.open callback
           getData();
         } else {
           console.log("Error opening serial port - " + error);
@@ -82,6 +90,9 @@ if (!device) {
       });
     } else {
       console.log('Error creating file - ' + fd + ' Error - ' + err);
-    };
+    }
   });
+} else {
+  console.log('You must specify the absolute path to a serial port that exists. For example: /dev/ttyO0');
+  process.exit();
 };
